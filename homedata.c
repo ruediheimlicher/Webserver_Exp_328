606c25bf65c1a4143160ebd48a8d6bf07c0bea66
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2014-07-29 10:54:21 +0200

    xxx

---
+++ homedata.c
@@ -0,0 +1,26 @@
+/*
+ *  homedata.c
+ *  WebServer
+ *
+ *  Created by Sysadmin on 15.Oktober.09.
+ *  Copyright 2009 Ruedi Heimlicher. All rights reserved.
+ *
+ */
+
+#include "homedata.h"
+
+#ifndef DATENBREITE
+#define DATENBREITE 8
+#endif
+
+extern volatile uint8_t txstartbuffer;
+extern volatile uint8_t txbuffer[];
+
+
+uint8_t readEEPROM_Tagplan(uint8_t* buffer)
+{
+	uint8_t erfolg=0;
+	
+
+	return erfolg;
+}
---
+++ homedata.h
@@ -0,0 +1,10 @@
+/*
+ *  homedata.h
+ *  WebServer
+ *
+ *  Created by Sysadmin on 15.Oktober.09.
+ *  Copyright 2009 Ruedi Heimlicher. All rights reserved.
+ *
+ */
+
+uint8_t readEEPROM_Tagplan(uint8_t* buf);
